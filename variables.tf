7e96acc4b045f17f3473de892014f2fdf1e20ccc
az900spallawkar <66231134+az900spallawkar@users.noreply.github.com>
2020-09-15 15:50:01 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,20 +1,20 @@
 variable "region" {
-  type    = "string"
+  type    = string
   default = "eu-west-2"
 }
 variable "ami_id" {
   type = "map"
   default = {
    # us-east-1    = "ami-035b3c7efe6d061d5"
-    eu-west-2    = "ami-132b3c7efe6sdfdsfd"
+    eu-west-2    = "ami-0287acb18b6d8efff"
    # eu-central-1 = "ami-9787h5h6nsn75gd33"
   }
 }
 variable "instance_type" {
-  type    = "string"
+  type    = string
   default = "t2.micro"
 }
 variable "key_name" {
-  type    = "string"
+  type    = string
   default = "jenkinskey"
 }
